8ae2960222a509f1ffe536446aacedd86c1fad03
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-24 12:05:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => renderBooks(data));
 }
-
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
